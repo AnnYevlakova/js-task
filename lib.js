84fc8674306d6a96fb78e6578447a8775605b5a3
AnnYevlakova <ann.yevlakova@gmail.com>
2017-07-12 20:06:10 +0300

    implemented method chain

--- lib.js
+++ lib.js
@@ -1,39 +1,90 @@
 ;(function() {
-    let library = {};
+    const library = {};
+  
     library.forEach = function(arr,callback) {
-        for (let i= 0; i <arr.length; i++) {
+        if(callback == undefined) {
+            callback = arr;
+            arr = this;
+        }
+        for (let i = 0; i < arr.length; i++) {
             callback(arr[i], i, arr);
         }
     };
+  
     library.filter = function(arr,callback) {
+        if(callback == undefined) {
+            callback = arr;
+            arr = this;
+        }
         let newArr = [];
-        for (let i= 0; i <arr.length; i++) {
+        for (let i = 0; i < arr.length; i++) {
             if(callback(arr[i], i, arr)) {
               newArr.push(arr[i]);
             }
         }
+        if(arr.take != undefined) {
+            library.chain(newArr);
+        }
         return newArr;
     };
+  
     library.reduce = function(arr, callback) {
+        if(callback == undefined) {
+            callback = arr;
+            arr = this;
+        }
         let result = arr[0];
-        for (let i= 1; i <arr.length; i++) {
+        for (let i = 1; i < arr.length; i++) {
             result = callback(result, arr[i], i, arr); 
         }     
         return result;
     };
+  
     library.map = function(arr, callback) {
-      let newArr = [];
-        for (let i= 0; i <arr.length; i++) {
+        if(callback == undefined) {
+            callback = arr;
+            arr = this;
+        }
+        let newArr = [];
+        for (let i = 0; i < arr.length; i++) {
             newArr.push(callback(arr[i], i, arr));
         }
-      return newArr;
+        if(arr.take != undefined) {
+            library.chain(newArr);
+        }
+        return newArr;
     };
+  
     library.take = function(arr, n) {
-        return arr[n];
+        if(n == undefined) {
+            n = arr;
+            arr = this;
+        }
+        let newArr = arr.slice(0,n);
+        if(arr.take != undefined) {
+            library.chain(newArr);
+        }
+        return newArr;
     };
+  
     library.skipe = function(arr, n) {
+        if(n == undefined) {
+            n = arr;
+            arr = this;
+        }
         arr.splice(n,1);
         return arr;
     };
+  
+    library.chain = function(a) {
+        a.take = this.take;
+        a.skipe = this.skipe;
+        a.map = this.map;
+        a.reduce = this.reduce;
+        a.filter = this.filter;
+        a.forEach = this.forEach;
+        return a;
+    }
+  
     window.lib = library;
 }());
--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 console.log(lib.filter([2,4,5,1],(a)=>{
   return a>3;
 }));
-console.log(lib.forEach([2,4,5,1],(a)=>{
+lib.forEach([2,4,5,1],(a)=>{
   alert(a);
-}));
+});
 console.log(lib.reduce([2,4,5,1], (a,b)=>{
   return a+b;
 }));
+
 console.log(lib.map([2,4,5,1], (a)=>{
   return a+1;
 }));
-console.log(lib.take([2,4,5,1],3));
+console.log(lib.take([2,4,5,1],2));
 console.log(lib.skipe([2,4,5,1],3));
+console.log(lib.chain([2,4,5,1]).take(2).skipe(1));
